b524ec661224b6db532fb36184b79c80b9c8b0bf
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-03-04 21:13:49 +0530

    Create diamond.java

---
+++ diamond.java
@@ -0,0 +1,31 @@
+public class patterns{
+public static void diamond(int n){
+        //half diamond
+        for(int i=1;i<=n;i++){
+            //starting spaces 
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //2nd half
+        for(int i=n;i>=1;i--){
+            //starting spaces 
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+public static void main(String[] args){
+  diamond(4);
+}
+}
